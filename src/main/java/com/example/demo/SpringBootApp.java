696ea57c9dad6ab1710f6100f514a305d5a95d82
Sudhir Yadav <sudhir.yadav@ideas.com>
2023-04-29 21:06:38 +0530

    Protect private end point whereas expose public one

--- src/main/java/com/example/demo/SpringBootApp.java
+++ src/main/java/com/example/demo/SpringBootApp.java
@@ -2,9 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class SpringBootApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApp.class, args);
