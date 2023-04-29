15985f1128d7f27636c656e6b4b942d86e6668d0
Sudhir Yadav <sudhir.yadav@ideas.com>
2023-04-29 21:08:51 +0530

    Add form login to authenticate use with default creted password

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.example.demo;
 import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -17,10 +20,12 @@ public class SecurityConfig {
         return httpSecurity
                 .authorizeRequests(
                         configurer -> {
+                            configurer.antMatchers("/favicon.ico").permitAll();
                             configurer.antMatchers("/public").permitAll();
                             configurer.anyRequest().authenticated();
                         }
                 )
+                .formLogin(withDefaults())
                 .build();
     }
 }
